01c31f931b763d532e812e56648574d00d77c5ee
fha96 <fahadalrimawe96@hotmail.com>
2022-08-04 23:07:57 +0300

    add modify movies module by add cache

--- modules/movies.js
+++ modules/movies.js
@@ -1,19 +1,32 @@
 // https://api.themoviedb.org/3/search/movie?api_key=140ed5bb0c130efde754d62530d0cc1e&query=amman   
 const axios=require('axios');
+
+const moviesCache={};
 async function movieHandler(request,response){
     let cityName=request.query.cityName;
     const url=`https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${cityName}`;
-    try {
-        const moviesData= await axios.get(url);
-        console.log(moviesData);
-        const arrayOfMoviesData=moviesData.data.results.map(item=>{
-            return new MovieApi(item);
-        });
+    console.log(moviesCache[cityName]);
+    if(moviesCache[cityName] !== undefined){
+        response.status(200).send(moviesCache[cityName]);
+    }else{
+        try {
+            const moviesData= await axios.get(url);
+            // console.log(moviesData);
+            const arrayOfMoviesData=moviesData.data.results.map(item=>{
+                return new MovieApi(item);
+            });
+            moviesCache[cityName]=arrayOfMoviesData;
+            response.status(200).send(arrayOfMoviesData);
+        } catch (error) {
+           errorHandler(error,response);
+        }
 
-        response.status(200).send(arrayOfMoviesData);
-    } catch (error) {
-       errorHandler(error,response);
+
     }
+    
+    
+    
+    
 
 };
 class MovieApi{
